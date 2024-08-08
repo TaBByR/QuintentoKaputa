99e3d8df80c13e3134fcb1510978e22b4d165b63
Henrique <henriquemmceleste@gmail.com>
2024-08-08 06:44:23 +0100

    a lot of shit

---
+++ js/service/chooseCountryService.js
@@ -0,0 +1,17 @@
+const API_URL = "https://flagcdn.com/en/codes.json";
+
+const IMG_SIZE = "80x60";
+
+export async function getCountries() {
+    const response = await fetch(API_URL);
+
+    if(!response.ok) {
+        console.log("fodeu");
+    }
+
+    return response.json();
+}
+
+export function getCountryImg(key) {
+    return `https://flagcdn.com/${IMG_SIZE}/${key}.png`;
+}
